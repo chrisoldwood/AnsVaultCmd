92b0a8cd474987867240110ad98aeac634562f94
Chris Oldwood <gort@cix.co.uk>
2017-10-14 23:06:43 +0100

    Added initial support for the help and version switches.

--- Source/Tests/ProgramTests.cs
+++ Source/Tests/ProgramTests.cs
@@ -1,4 +1,5 @@
-﻿using Mechanisms.Tests;
+﻿using System.IO;
+using Mechanisms.Tests;
 
 namespace Tests
 {
@@ -7,14 +8,56 @@ namespace Tests
         [TestCases]
         public static void exit_code()
         {
-            "A succeessful innvocation returns 0".Is(() =>
+            "An innvocation with no arguments returns non-zero".Is(() =>
             {
                 var successfulInnvocationArgs = new[] { "" };
 
-                var exitCode = VaultCmd.Program.AppMain(successfulInnvocationArgs);
+                var exitCode = VaultCmd.Program.AppMain(successfulInnvocationArgs, AnyStdIn, AnyStdOut, AnyStdErr);
+
+                Assert.True(exitCode != 0);
+            });
+
+            "Requesting help returns 0".Is(() =>
+            {
+                var helpArg = new[] { "--help" };
+
+                var exitCode = VaultCmd.Program.AppMain(helpArg, AnyStdIn, AnyStdOut, AnyStdErr);
 
                 Assert.True(exitCode == 0);
             });
+
+            "Requesting help outputs a usage message".Is(() =>
+            {
+                var helpArg = new[] { "--help" };
+                var stdOut = new StringWriter();
+
+                VaultCmd.Program.AppMain(helpArg, AnyStdIn, stdOut, AnyStdErr);
+
+                Assert.True(stdOut.ToString().Contains("Usage"));
+            });
+
+            "Requesting the version returns 0".Is(() =>
+            {
+                var versionArg = new[] { "--version" };
+
+                var exitCode = VaultCmd.Program.AppMain(versionArg, AnyStdIn, AnyStdOut, AnyStdErr);
+
+                Assert.True(exitCode == 0);
+            });
+
+            "Requesting the version outputs a version string".Is(() =>
+            {
+                var versionArg = new[] { "--version" };
+                var stdOut = new StringWriter();
+
+                VaultCmd.Program.AppMain(versionArg, AnyStdIn, stdOut, AnyStdErr);
+
+                Assert.True(stdOut.ToString().Contains("0.0.0.1"));
+            });
         }
+
+        private static readonly TextReader AnyStdIn = new StringReader("");
+        private static readonly TextWriter AnyStdOut = new StringWriter();
+        private static readonly TextWriter AnyStdErr = new StringWriter();
     }
 }
--- Source/VaultCmd/Program.cs
+++ Source/VaultCmd/Program.cs
@@ -1,4 +1,7 @@
-﻿using Mechanisms.Host;
+﻿using System;
+using System.IO;
+using System.Reflection;
+using Mechanisms.Host;
 
 namespace VaultCmd
 {
@@ -11,7 +14,35 @@ namespace VaultCmd
 
         internal static int AppMain(string[] args)
         {
-            return ExitCode.Success;
+            return AppMain(args, Console.In, Console.Out, Console.Error);
+        }
+
+        internal static int AppMain(string[] args, TextReader stdin, TextWriter stdout, TextWriter stderr)
+        {
+            const int help = 1;
+            const int version = 2;
+
+            var switches = new[]
+            {
+                new Switch(help, "h", "help"),
+                new Switch(version, Switch.NoShortName, "version"),
+            };
+
+            var arguments = CommandLineParser.Parse(args, switches);
+
+            if (arguments.IsSet(help))
+            {
+                stdout.WriteLine("Usage: VaultCmd [options...]");
+                return ExitCode.Success;
+            }
+
+            if (arguments.IsSet(version))
+            {
+                stdout.WriteLine(Assembly.GetExecutingAssembly().GetName().Version.ToString());
+                return ExitCode.Success;
+            }
+
+            return ExitCode.Failure;
         }
     }
 }
